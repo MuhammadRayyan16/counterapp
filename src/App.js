9d404384a8ef94b1109de13e0cbae8e48d43b2e6
Rayyan7878 <rayyanshaikh787898@gmail.com>
2024-06-10 08:21:25 -0700

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Confetti from 'react-confetti'
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='container'>
+      {count >= 10 &&
+        <Confetti
+          width={1350}
+          height={580} />
+      }
+      <h1>Counter</h1>
+      <div id='goal'>
+        <h1>{count}</h1>
+      </div>
+      <div id="center">
+        <div id='first'>
+          <button onClick={() => setCount(count + 1)}>Increment +</button>
+        </div>
+        <div id='secound'>
+          <button onClick={() => setCount(count - 1)}>Decrement -</button>
+        </div>
+      </div>
     </div>
   );
 }
